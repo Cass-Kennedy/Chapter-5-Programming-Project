4d4fcc2d6dab39e32d528a95993d6abfe4f71c8d
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 11:10:51 -0400

    This was just a starting thing to see if I could get it to run.

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -1,29 +1,10 @@
-// This program is a rudimentary way to calculate the expected population after a certain number of days.
+// This program is meant to count the number of students in class and determine which should be at the end.
 
 #include <iostream>
-#include <iomanip>
+#include <fstream>
 using namespace std;
 int main()
 {
-    float populationCount, days, avgGrowth; //Starting population, amount of time, and average growth rate for the organisms
-    cout << "How many organisms are in the original population? ";
-    cin >> populationCount;
-    cout << "What is the average rate of growth for the organisms as a percentage? ";
-    cin >> avgGrowth;
-    cout << "How many days does this group grow? ";
-    cin >> days;
-    if (avgGrowth > 0 && avgGrowth < 1)
-    {
-        avgGrowth += 1;
-    };
-    if (populationCount < 2 || days < 1 || avgGrowth < 0) {
-        cout << "Please enter a valid population, time, and growth rate.";
-    }
-    else {
-        for (int i = 0; i <= days; i++) {
-            cout << " The population on day " << i << " is " << populationCount << endl;
-            populationCount *= avgGrowth;
-        };
-    } 
+    cout << "Hi.";
     return 0;
 }
