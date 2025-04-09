e78783c2c47e24154504b201001ce99d67c4f483
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 12:07:42 -0400

    Done most of the program, however I need to figure out how to count odd numbers and how to make sure everyone can use the program, not just someone with the same user name as me.

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -2,9 +2,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 int main()
 {
-    cout << "Hi.";
+    int counter = 0;
+    string filename, lineA, lineB, finalName;
+    filename = "C:\\Users\\aidan\\Downloads\\LineUp.txt";
+    ifstream inFile(filename);
+    if (!inFile)
+    {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+    while (inFile.peek() != EOF)
+    {
+        getline(inFile, lineA);
+        getline(inFile, lineB);
+        counter+=2; //Because the program reads twice, counter needs to update by two.
+        if (lineA < lineB)
+        {
+            finalName = lineB;
+        }
+        else {
+            finalName = lineA;
+        };
+    };
+    cout << "There are " << counter << " children in the class" << endl;
+    cout << finalName << " is the last kid in line." << endl;
+    inFile.close();
     return 0;
 }
