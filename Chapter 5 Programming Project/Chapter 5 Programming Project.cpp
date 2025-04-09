ed01d88044f8b17c12ba6fa00671f28fd6975d64
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 11:10:12 -0400

    Added some parts the program to do with calculating the population.

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -2,10 +2,12 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 int main()
 {
-    float populationCount, days, avgGrowth; //Starting population, amount of time, and average growth rate for the organisms
+    float populationCount, days, avgGrowth,euler; //Starting population, amount of time, and average growth rate for the organisms
+    euler = 2.71828;
     cout << "How many organisms are in the original population? ";
     cin >> populationCount;
     cout << "What is the average rate of growth for the organisms as a percentage? ";
@@ -16,13 +18,17 @@ int main()
     {
         avgGrowth += 1;
     };
-    if (populationCount < 2 || days < 1 || avgGrowth < 0) {
+    if (avgGrowth > 1 && avgGrowth <= 100)
+    {
+        avgGrowth = (avgGrowth / 100.00) + 1;
+    };
+    if (populationCount < 2 || days < 1 || avgGrowth < 0 ) {
         cout << "Please enter a valid population, time, and growth rate.";
     }
     else {
         for (int i = 0; i <= days; i++) {
             cout << " The population on day " << i << " is " << populationCount << endl;
-            populationCount *= avgGrowth;
+            populationCount *= pow(euler, avgGrowth);
         };
     } 
     return 0;
