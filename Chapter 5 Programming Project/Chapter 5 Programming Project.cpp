8a30ae202e1dcb35acbffd04c92a422096d19d6c
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 16:22:53 -0400

    Added a way to determine the first in line as well.

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     int counter = 0;
-    string filename, lineA, lineB, lineC, finalName;
+    string filename, lineA, lineB, lineC, firstName, finalName;
     filename = "Lineup.txt";
     ifstream inFile(filename);
     if (inFile)
@@ -18,23 +18,30 @@ int main()
     if (inFile.peek() != EOF) {
         getline(inFile, lineA);
         getline(inFile, lineB);
-        counter += 2; //Because the program reads twice, counter needs to update by two.
+        counter += 2; //Because the program reads twice to start, counter needs to update by two.
         if (lineA < lineB)
         {
             finalName = lineB;
+            firstName = lineA;
         }
         else {
             finalName = lineA;
+            firstName = lineB;
         };
+    };
     while (inFile.peek() != EOF)
     {
         getline(inFile, lineC);
         if (lineC < finalName) {
-            finalName = lineC;
+            finalName = lineC; 
         };
-        counter += 1;
+        if (lineC > firstName) {
+            firstName = lineC;
+        }
+        counter += 1; //Adds one to the number of children each loop.
     };
     cout << "There are " << counter << " children in the class" << endl;
+    cout << firstName << " is the first kid in line." << endl;
     cout << finalName << " is the last kid in line." << endl;
     inFile.close();
     return 0;
