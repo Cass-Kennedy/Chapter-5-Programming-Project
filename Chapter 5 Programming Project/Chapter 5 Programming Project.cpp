bfc4eb603f6bfba3d824591a61039e7e5d132ea7
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 14:26:47 -0400

    HAHA

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -6,15 +6,6 @@
 using namespace std;
 int main()
 {   
-    int index = 9;
-    while (index <= 0)
-    {
-        for (int count = index; count >= 0; count--)
-        {
-            cout << "+";
-        };
-        cout << endl;
-        --index;
-    };
+    
     return 0;
 }
