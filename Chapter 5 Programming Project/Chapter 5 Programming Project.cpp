abd794581ffb7fe6944e784520d5f857183b15a2
Cass-Kennedy <alkennedy228@gmail.com>
2025-04-09 15:08:40 -0400

    Changed the formula somewhat.

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -2,33 +2,31 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
 int main()
 {
-    float populationCount, days, avgGrowth,euler; //Starting population, amount of time, and average growth rate for the organisms
-    euler = 2.71828;
+    float populationCount, days, avgGrowth; //Starting population, amount of time, and average growth rate for the organisms
     cout << "How many organisms are in the original population? ";
     cin >> populationCount;
     cout << "What is the average rate of growth for the organisms as a percentage? ";
     cin >> avgGrowth;
     cout << "How many days does this group grow? ";
     cin >> days;
+    if (avgGrowth > 1 && avgGrowth <= 100)
+    {
+        avgGrowth = (avgGrowth / 100.00);
+    };
     if (avgGrowth > 0 && avgGrowth < 1)
     {
         avgGrowth += 1;
     };
-    if (avgGrowth > 1 && avgGrowth <= 100)
-    {
-        avgGrowth = (avgGrowth / 100.00) + 1;
-    };
-    if (populationCount < 2 || days < 1 || avgGrowth < 0 ) {
+    if (populationCount < 2 || days < 1 || avgGrowth < 0 || avgGrowth > 100) {
         cout << "Please enter a valid population, time, and growth rate.";
     }
     else {
         for (int i = 0; i <= days; i++) {
             cout << " The population on day " << i << " is " << populationCount << endl;
-            populationCount *= pow(euler, avgGrowth);
+            populationCount = populationCount*avgGrowth;
         };
     } 
     return 0;
